2fe8c65bf4ff700af8da0421f0a1141d0572cb41
Matthias Klumpp <matthias@tenstral.net>
2024-01-14 23:46:38 +0100

    trivial: post release version bump

--- spark/__init__.py
+++ spark/__init__.py
@@ -15,5 +15,5 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this software.  If not, see <http://www.gnu.org/licenses/>.
 
-__appname__ = "laniakea-spark"
-__version__ = "0.1"
+__appname__ = 'laniakea-spark'
+__version__ = '0.2'
